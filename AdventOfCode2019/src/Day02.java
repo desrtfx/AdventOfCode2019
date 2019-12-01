d4b5462500f8a05eecf8c9e19585f10ec32aef5d
desrtfx <desrtfx@gmail.com>
2019-12-01 14:46:26 +0100

    Preparation for Day02

---
+++ AdventOfCode2019/src/Day02.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class Day02 {
+
+	static List<String> data;
+	
+	static {
+		String fileName = "Input\\Input_Day02.txt";
+
+		data = FileIO.getFileAsList(fileName);
+	}
+	
+	public static int part01(List<String> data) {
+	
+		return 0;
+	}
+	
+	public static int part02(List<String> data) {
+		
+		return 0;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Part 1 result: %d%n", part01(data));
+		System.out.printf("Part 2 result: %d%n", part02(data));
+
+	}
+
+}
