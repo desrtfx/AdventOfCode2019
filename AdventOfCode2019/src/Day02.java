2f2e125515792aad6b24c57d01fa50a87061e006
desrtfx <desrtfx@gmail.com>
2019-12-02 20:40:56 +0100

    Day02 complete

--- AdventOfCode2019/src/Day02.java
+++ AdventOfCode2019/src/Day02.java
@@ -2,21 +2,60 @@ import java.util.List;
 
 public class Day02 {
 
-	static List<String> data;
+	static int[] data;
 	
 	static {
 		String fileName = "Input\\Input_Day02.txt";
 
-		data = FileIO.getFileAsList(fileName);
+		String rawData = FileIO.getFileAsString(fileName);
+		String[] rawArray = rawData.split(",");
+		data = new int[rawArray.length];
+		System.out.println(rawArray.length);
+		for(int i = 0; i < rawArray.length; i++) {
+			data[i] = Integer.parseInt(rawArray[i]);
+		}
 	}
 	
-	public static int part01(List<String> data) {
-	
-		return 0;
+	public static int compute(int[] data, int reg1, int reg2) {
+		int[] myData = new int[data.length];
+		for(int i=0; i < data.length; i++) {
+			myData[i] = data[i];
+		}
+		myData[1] = reg1;
+		myData[2] = reg2;
+		int ip = 0;
+		int op = myData[ip];
+		while (op != 99) {
+			switch(op) {
+			case 1: 
+				myData[myData[ip + 3]] = myData[myData[ip + 1]] + myData[myData[ip + 2]];
+				break;
+			case 2:
+				myData[myData[ip + 3]] = myData[myData[ip + 1]] * myData[myData[ip + 2]];
+				break;
+			default:
+				System.err.print("Something went seriously wrong!");
+				break;
+			}
+			ip+=4;
+			op = myData[ip];
+		}
+		return myData[0];
 	}
 	
-	public static int part02(List<String> data) {
-		
+	
+	public static int part01(int[] data) {
+		return compute(data, 12, 2);
+	}
+	
+	public static int part02(int[] data) {
+		for(int x = 0; x < 100; x++) {
+			for(int y = 0; y < 100; y++) {
+				if(compute(data, x,y) == 19690720) {
+					return 100 * x + y;
+				}
+			}
+		}
 		return 0;
 	}
 	
